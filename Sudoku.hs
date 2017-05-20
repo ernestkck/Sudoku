1cd2a50353c8fef2ca11b658e0e015df6a5b08a4
Ernest Kwan <u6381103@anu.edu.au>
2017-05-21 00:31:15 +1000

    complete allBlanks

--- Sudoku.hs
+++ Sudoku.hs
@@ -20,7 +20,7 @@ module Sudoku
   ) where
 
 import Test.QuickCheck
-
+import Data.List
 -- A matrix is a list of rows.
 type Matrix a = [Row a]
 
@@ -41,101 +41,20 @@ type Cell = Maybe Int
 example :: Sudoku
 example =
   Sudoku
-    [ [ Just 3
-      , Just 6
-      , Nothing
-      , Nothing
-      , Just 7
-      , Just 1
-      , Just 2
-      , Nothing
-      , Nothing
-      ]
-    , [ Nothing
-      , Just 5
-      , Nothing
-      , Nothing
-      , Nothing
-      , Nothing
-      , Just 1
-      , Just 8
-      , Nothing
-      ]
-    , [ Nothing
-      , Nothing
-      , Just 9
-      , Just 2
-      , Nothing
-      , Just 4
-      , Just 7
-      , Nothing
-      , Nothing
-      ]
-    , [ Nothing
-      , Nothing
-      , Nothing
-      , Nothing
-      , Just 1
-      , Just 3
-      , Nothing
-      , Just 2
-      , Just 8
-      ]
-    , [ Just 4
-      , Nothing
-      , Nothing
-      , Just 5
-      , Nothing
-      , Just 2
-      , Nothing
-      , Nothing
-      , Just 9
-      ]
-    , [ Just 2
-      , Just 7
-      , Nothing
-      , Just 4
-      , Just 6
-      , Nothing
-      , Nothing
-      , Nothing
-      , Nothing
-      ]
-    , [ Nothing
-      , Nothing
-      , Just 5
-      , Just 3
-      , Nothing
-      , Just 8
-      , Just 9
-      , Nothing
-      , Nothing
-      ]
-    , [ Nothing
-      , Just 8
-      , Just 3
-      , Nothing
-      , Nothing
-      , Nothing
-      , Nothing
-      , Just 6
-      , Nothing
-      ]
-    , [ Nothing
-      , Nothing
-      , Just 7
-      , Just 6
-      , Just 9
-      , Nothing
-      , Nothing
-      , Just 4
-      , Just 3
-      ]
+    [ [ Just 3, Just 6, Nothing, Nothing, Just 7, Just 1, Just 2, Nothing, Nothing]
+    , [ Nothing, Just 5, Nothing, Nothing, Nothing, Nothing, Just 1, Just 8, Nothing]
+    , [ Nothing, Nothing, Just 9, Just 2, Nothing, Just 4, Just 7, Nothing, Nothing]
+    , [ Nothing, Nothing, Nothing, Nothing, Just 1, Just 3, Nothing, Just 2, Just 8]
+    , [ Just 4, Nothing, Nothing, Just 5, Nothing, Just 2, Nothing, Nothing, Just 9]
+    , [ Just 2, Just 7, Nothing, Just 4, Just 6, Nothing, Nothing, Nothing, Nothing]
+    , [ Nothing, Nothing, Just 5, Just 3, Nothing, Just 8, Just 9, Nothing, Nothing]
+    , [ Nothing, Just 8, Just 3, Nothing, Nothing, Nothing, Nothing, Just 6, Nothing]
+    , [ Nothing, Nothing, Just 7, Just 6, Just 9, Nothing, Nothing, Just 4, Just 3]
     ]
 
 -- allBlanks is a Sudoku with just blanks
 allBlanks :: Sudoku
-allBlanks = undefined -- TODO
+allBlanks = Sudoku (replicate 9 (replicate 9 Nothing))
 
 -- | isSudoku checks if a Sudoku has the proper dimensions
 -- >>> isSudoku (Sudoku [])
